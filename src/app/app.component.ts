aaa5a363bf3133ec38371048a9b9dc8527f624fc
Lucas <lucassoares.dev@hotmail.com>
2024-12-25 17:29:14 -0300

    Iniciando o projeto, criando a primeira página com html e css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePrincipalComponent } from './home-principal/home-principal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomePrincipalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/home-principal/home-principal.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-principal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home-principal.component.html',
+  styleUrl: './home-principal.component.css'
+})
+export class HomePrincipalComponent {
+
+}
